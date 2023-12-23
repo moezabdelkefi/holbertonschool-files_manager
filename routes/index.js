967c9ac7738d93cb263d6659c7eb4193afff265a
moezabdelkef <moezbadelkefi17@gmail.com>
2023-12-23 20:04:32 +0100

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
-const router = express.Router();
+const router = (app) => {
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-
+  router.get('/status', AppController.getStatus);
+  router.get('/stats', AppController.getStats);
+  router.post('/users', UsersController.postNew);
+  paths.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+  paths.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+};
 module.exports = router;
